ed2d593eec046a7b359176501da496354dc98b42
Vishwanath <vishwasharma300@gmail.com>
2026-06-27 00:58:49 +0530

    feat: add real-time monitoring engine

---
+++ src/monitors/realtime_monitor.py
@@ -0,0 +1,15 @@
+"""
+Real-Time Monitoring
+"""
+
+import time
+
+def monitor():
+
+    print("Starting real-time monitoring...")
+
+    while True:
+
+        print("Monitoring files...")
+
+        time.sleep(5)
